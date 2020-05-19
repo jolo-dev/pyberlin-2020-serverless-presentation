54bb255924f31ca1f9ffc7fce52689a0cb861404
John Long <johnyscrazy@gmail.com>
2020-05-20 00:50:14 +0200

    Add more slides

--- static/js/script.js
+++ static/js/script.js
@@ -1,1 +1,27 @@
 const ws = new WebSlides()
+hljs.initHighlightingOnLoad()
+
+const animate = document.getElementsByClassName('animate')
+for (let i = 0; i < animate.length; i++) {
+  console.log(animate[i])
+  let content = ''
+  for (let x = 0; x < animate[i].textContent.length; x++) {
+    let char = animate[i].textContent.charAt(x)
+    if (char !== ' ') {
+      content += `<span>${char}</span>`
+    } else {
+      content += `<span style="width:2%;height:4px">${char}</span>`
+    }
+  }
+  animate[i].innerHTML = content
+}
+
+ws.el.addEventListener('ws:slide-change', function (slide) {
+  if (slide.detail.currentSlide == 3) {
+    var a = document.getElementById('social-media')
+    setTimeout(() => {
+      a.classList.add('animate')
+      a.style.display = 'block'
+    }, 1500)
+  }
+})
