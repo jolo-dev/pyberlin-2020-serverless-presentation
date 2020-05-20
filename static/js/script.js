1dba3e9670ba5fe59117cb81d72aac69bed80568
John Long <johnyscrazy@gmail.com>
2020-05-20 12:23:06 +0200

    Add more code

--- static/js/script.js
+++ static/js/script.js
@@ -17,7 +17,7 @@ for (let i = 0; i < animate.length; i++) {
 }
 
 ws.el.addEventListener('ws:slide-change', function (slide) {
-  if (slide.detail.currentSlide == 3) {
+  if (slide.detail.currentSlide == 2) {
     var a = document.getElementById('social-media')
     setTimeout(() => {
       a.classList.add('animate')
